9e3ab686d2c4fadaa3f3206e349f8e673270de0b
Alexis <alexismattaruco83@gmail.com>
2024-07-10 15:35:06 -0300

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+const port = 3000;
+
+require('dotenv').config();
+
+// Configuración de la conexión a la base de datos
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
+
+// Conectar a la base de datos
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to the MySQL database.');
+});
+
+// Ruta de prueba
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
