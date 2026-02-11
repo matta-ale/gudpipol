b0a203ade323aa660c593d2f1a49e07f3ba22c4a
Alexis <alexismattaruco83@gmail.com>
2026-02-11 19:44:03 -0300

    agregado de configuración de sequelize para que no duerma el backend

--- server/config/database.js
+++ server/config/database.js
@@ -5,7 +5,17 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  dialect: 'mysql'
+  dialect: 'mysql',
+  dialectOptions: {
+    // Esto ayuda con problemas de red en Railway
+    connectTimeout: 60000 
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000, // Tiempo máximo para intentar conectar
+    idle: 10000    // Tiempo antes de liberar una conexión inactiva
+  }
 });
 
 module.exports = sequelize;
